b3598e91bd143441cddef48fd56faad5805d0b45
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 <l.leroty@gmail.com>
2022-11-29 15:48:21 +0100

    add the task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,14 +10,16 @@ app = Flask(__name__)
 
 
 # Declare a method to handle @app.teardown_appcontext
-@app.teardown_appcontext
-def close(self):
-    storage.close()
 
 @app.route('/states_list', strict_slashes=False)
 def display_states(self):
     all_states = self.storage.all(State)
     return render_template('7-states.html', all_states=all_states)
+
+
+@app.teardown_appcontext
+def close(self):
+    storage.close()
 
 
 if __name__ == "__main__":
